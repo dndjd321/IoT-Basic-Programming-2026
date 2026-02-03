ca0ab12c5a01d71eca3ece5c6603099faf0f8400
dndjd321 <rnqudwns2515@gmail.com>
2026-02-03 13:36:15 +0900

    2026-02-03 first commit

---
+++ DAY02/Chapter03_Solution/Chapter03_01_app/main.c
@@ -0,0 +1,62 @@
+﻿// Chapter03_01_app/main.c
+// 변수
+
+#include <stdio.h>	// 표준 입출력 헤더 파일
+#include <limits.h>	// 타입의 최소, 최대값을 담고 있는 헤더 파일
+
+int main() {
+	// 변수 선언
+	int a;				// 정수형 변수 a 선언
+	int b, c;			// 정수형 변수 b, c 선언
+	float f;			// 실수형 변수 f 선언
+	double d;			// 더 정밀한 실수형 변수 d 선언
+	char ch;			// 문자형 변수 ch 선언
+	
+	// assignments(대입) 왼쪽의 변수에 오른쪽에는 값을 넣음.
+	// 변수에 값 할당
+	a = 10;				// 변수 a에 10 할당
+	b = a;				// 변수 b에 20 할당
+	c = a + 20;			// 변수 c에 a와 b의 합 할당
+	//c = 3.145;		// Ctrl + / : 주석 처리
+	f = 3.14159f;		// 변수 f에 3.14 할당
+	d = 3.1415926535;	// 변수 d에 3.1415926535 할당
+	ch = 'A';			// 변수 ch에 문자 'A' 할당
+
+	// 변수 값 출력
+	printf("변수 a = %d\n", a);		// 변수 a 출력
+	printf("변수 b = %d\n", b);		// 변수 b 출력
+	printf("변수 c = %d\n", c);		// 변수 c 출력
+	
+	// '%f'는 소수점 6자리까지 출력, '%.nf'는 소수점 n자리까지 출력
+	printf("변수 f = %f\n", f);		// 변수 f 출력
+	printf("변수 d = %.10f\n", d);	// 변수 d 출력
+
+	// '%c'는 문자 하나를 출력, '%s'는 문자열 출력
+	printf("변수 ch = %c\n", ch);	// 변수 ch 출력
+	printf("변수 ch = %d\n", ch);	// 변수 ch 출력 ( 아스키 코드 값으로 출력 )
+
+	// ASCII - 영문자, 특수문자(+,-,etc.) 등 숫자값으로 1:1 매칭되어 있음.
+	// https://ko.wikipedia.org/wiki/ASCII - ASCII 코드 표
+	printf("변수 ch = %c\n", 66);	// 'B' 출력
+	printf("변수 ch = %c\n", 97);	// 'a' 출력
+
+	// 정수형
+	// char : 크키 1 byte -> 8bit 0000 0000 ~ 1111 1111 -> 00 ~ FF(0~255 까지 표현)
+	// short : 크기 2 bytes
+	// int(기본) : 크기 4 bytes
+	// long : 크기 4 bytes
+	// long long : 크기 8 bytes
+	printf("변수 char의 사이즈 : %zu byte, 최소 %d, 최대 %d\n", sizeof(char), CHAR_MIN, CHAR_MAX);
+	printf("변수 short의 사이즈 : %zu byte, 최소 %d, 최대 %d\n", sizeof(short), SHRT_MIN, SHRT_MAX);
+	printf("변수 int의 사이즈 : %zu byte, 최소 %d, 최대 %d\n", sizeof(int), INT_MIN, INT_MAX);
+	printf("변수 long long의 사이즈 : %zu byte, 최소 %lld, 최대 %lld\n", sizeof(long long), LLONG_MIN, LLONG_MAX);
+
+	// unsigned : 음수 표현 없이 양수와 0만 표현
+	printf("변수 unsigned char의 사이즈 : %zu byte, 최소 %d, 최대 %d\n", sizeof(unsigned char), 0, UCHAR_MAX);
+	printf("변수 unsigned short의 사이즈 : %zu byte, 최소 %d, 최대 %d\n", sizeof(unsigned short), 0, USHRT_MAX);
+	printf("변수 unsigned int의 사이즈 : %zu byte, 최소 %d, 최대 %u\n", sizeof(unsigned int), 0, UINT_MAX);
+	printf("변수 unsigned long의 사이즈 : %zu byte, 최소 %d, 최대 %u\n", sizeof(unsigned long), 0, ULONG_MAX);
+	printf("변수 unsigned long long의 사이즈 : %zu byte, 최소 %d, 최대 %llu\n", sizeof(unsigned long long), 0, ULLONG_MAX);
+
+	return 0;			// 프로그램 종료
+}
---
+++ DAY02/Chapter03_Solution/Chapter03_02_app/main.c
@@ -0,0 +1,59 @@
+﻿//Chapter03_02_app/main.c
+// 실수형
+
+#define _CRT_SECURE_NO_WARNINGS	// 보안 문제 이전의 오래된 함수사용을 위해서
+#include <stdio.h>	// 표준 입출력 헤더 파일
+#include <float.h>	// 실수형 타입의 최소, 최대값을 담고 있는 헤더 파일
+#include <string.h>	// 문자열 처리를 위한 라이브러리
+
+int	main(void) {
+	// float : 4bytes
+	// double(기본) : 8bytes
+	printf("변수 float, %zu byte, 최소 %e, 최대 %e\n", sizeof(float), FLT_MIN, FLT_MAX);
+	printf("변수 double, %zu byte, 최소 %e, 최대 %e\n", sizeof(double), DBL_MIN, DBL_MAX);
+
+	// 문자열 : 문자의 배열(char Array)
+	char fruit[20] = "strawberry";
+	printf("과일 이름 : %s\n", fruit);
+
+	// fruit = "banana"; 문자열에서 대입은 불가
+	// 대신 아래와 같이 함수를 사용해서 변수값을 변경
+	strcpy(fruit, "banana");
+	printf("변경된 과일 이름 : %s\n", fruit);
+
+	// const : 변하지 않는 값을 지정
+	const double tax_rate = 0.10;	// 세율
+	const double pi = 3.141592;		// 원주율
+	int income = 5000;
+	double tax = income * tax_rate;
+
+	printf("세금은 : %.0f원\n", tax);
+
+	//tax_rate = 0.20;	// const는 이후에 변경 불가
+
+
+	int age;	// unsigned char(최대값 255) 가 가장 적합
+	/*
+		-변수명 지정방식-
+		알파벳 A-Z, a-z, 0~9, '_' 까지만 허용
+		숫자로 시작할 수 없음
+		대소문자 구별
+		예약어는 변수명으로 사용 불가
+
+		의미있는 단어들의 조합으로 변수명을 선언할 것
+	*/
+
+	char full_name[50] = "asd";
+	int age = 50;
+	char address[100] = "부산";
+	char blood_type = 'A';
+	float height = 100.9f;
+
+
+
+
+	return 0;
+}
+
+
+
