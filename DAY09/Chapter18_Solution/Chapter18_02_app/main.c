8311e895b5d45ff8af3a8038232c272a08c617c7
dndjd321 <rnqudwns2515@gmail.com>
2026-02-12 12:32:07 +0900

    2026-02-12 12:32 commit

---
+++ DAY09/Chapter18_Solution/Chapter18_02_app/main.c
@@ -0,0 +1,52 @@
+﻿// Chapter18_02_app/main.c
+// 파일 입출력
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <direct.h>
+
+int main() {
+	char cwd[256];
+	char str[80];
+	char* res;
+
+	_getcwd(cwd, sizeof(cwd));
+	printf("현재 작업(프로젝트) 폴더 : %s\n", cwd);
+
+	// 여기서부터 txt파일 복사
+	FILE* fp, *ofp;
+	fp = fopen("./data/sample.txt", "r");
+	if (fp == NULL) {
+		printf("파일 열기 실패.\n");
+		return 1; //exit(1)과 동일한 기능
+	}
+	else {
+		printf("파일 열기 성공\n");
+	}
+
+	// 파일 복사용 오픈
+	ofp = fopen("./data/copy.txt", "w");
+	if (ofp == NULL) {
+		printf("출력파일 열기 실패.\n");
+		return 1; //exit(1)과 동일한 기능
+	}
+	
+	while (1) {
+		res = fgets(str, sizeof(str), fp);
+		if (res == NULL) {
+			break;
+		}
+
+		printf("한줄 쓰기 -> %s\n", str);
+		//str[strlen(str) - 1] = '\0';
+		fputs(str, ofp);
+		//fputs(" ", ofp);
+	}
+
+	fclose(fp);
+	fclose(ofp);
+
+	return 0;
+}
---
+++ DAY09/Chapter18_Solution/Chapter18_03_app/main.c
@@ -0,0 +1,54 @@
+﻿// Chapter18_03_app/main.c
+// 파일 입출력
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>	// OS 관련 또는 코드 페이지 인코딩 해결 
+
+int main() {
+	// console 출력, 입력을 UTF-8(65001)로 고정하기 위한 설정
+	SetConsoleOutputCP(65001);		// 콘솔 출력 코드페이지
+	SetConsoleCP(65001);			// 콘솔 자체 코드페이지
+
+	FILE* ifp, *ofp;
+
+	// 텍스트 내 포맷에 맞춰서 변수 선언
+	char name[20];
+	int kor, eng, math;
+	int total;
+	double avg;
+	int res;
+
+	ifp = fopen("./original.txt", "r");
+	if (ifp == NULL) {
+		printf(u8"입력파일 읽기 실패하였습니다.\n");	// u8 : 문자열을 utf8로 변환 출력하기위함.
+		exit(1);
+	}
+
+	ofp = fopen("./copy.txt", "w");	// w 쓰기 모드는 파일이 없으면 자동 생성
+
+	if (ifp == NULL) {
+		printf(u8"출력파일 생성 실패하였습니다.\n");
+		exit(1);
+	}
+
+	while (1) {
+		// 입력 파일 포인터에서 내용을 읽어서 한 줄 씩 각 변수에 할당하기
+		res = fscanf(ifp, "%s %d %d %d", name, &kor, &eng, &math);
+		if(res == EOF){	// 파일 데이터의 마지막까지 다 읽었을 경우 EOF 진입
+			break;
+		}
+
+		total = kor + eng + math;
+		avg = total / 3.0;
+
+		printf(u8"%s%5d%7.2lf\n", name, total, avg);	// console에 출력
+		fprintf(ofp, "%s%5d%7.2lf\n", name, total, avg);
+	}
+
+	printf(u8"파일 복사 완료.\n");
+	fclose(ifp);
+	fclose(ofp);
+
+	return 0;
+}
